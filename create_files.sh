21f145b642d258860ab2c95ac020dcc73cdfe1fd
Richard Bronosky <richard.bronosky@cmgdigital.com>
2010-08-13 17:51:15 -0400

    works for hash differences, but needs to be refactored

--- create_files.sh
+++ create_files.sh
@@ -1,1 +1,22 @@
-rm *.txt; c=0; while [[ $((c++)) -lt 20 ]]; do ls -la | gawk 'BEGIN{srand();};{r=rand();n=0;if(r<0.5 && r>=0.1){n=sprintf("%i", r*10)}; printf "%"n"s%s\n","",$0}' > $(printf '%04d' $c).txt; echo $c; sleep 2; done
+rm 00[0-2][0-9].txt 2>/dev/null
+c=0;
+n=$(date +%s);
+while [[ $((c++)) -lt 20 ]];
+do
+    echo -n "$c "
+    ls -la | \
+    gawk '
+        BEGIN {
+            srand('$((n+$((c*3))))');
+        }
+        {
+            r=rand();
+            n=0;
+            if(r<0.5 && r>=0.1){
+                n=sprintf("%i", r*10)
+            }
+            printf "%s%"n"s\n",$0,""
+        }
+    ' > $(printf '%04d' $c).txt;
+done
+echo "Done!"
--- whitespacer-fixer.sh
+++ whitespacer-fixer.sh
@@ -1,5 +1,15 @@
- git diff --cached>patch
- git apply --whitespace=fix -R patch
- git apply --whitespace=fix patch
- git reset HEAD $(sed '/^+++ /!d;s?^+++ b/??' patch)
- git add $(sed '/^+++ /!d;s?^+++ b/??' patch)
+ERR=$( git diff-index --cached --check HEAD | sed '/^\(\+\|-\)/d;s/:.*//' | sort | uniq )
+echo -e "Files in index with whitespace errors:\n$ERR"
+if [[ $# != 2 ]]; then
+    git diff --cached > patch
+else
+    git diff $* > patch
+fi
+git apply --whitespace=fix -R patch
+git apply --whitespace=fix patch
+git reset HEAD $(sed '/^+++ /!d;s?^+++ b/??' patch)
+git add $(sed '/^+++ /!d;s?^+++ b/??' patch)
+ERR=$( git diff-index --cached --check HEAD | sed '/^\(\+\|-\)/d;s/:.*//' | sort | uniq )
+if [[ $(echo "$ERR" | wc -l) -gt 0 ]]; then
+    echo -e "Remaining files in index with whitespace errors:\n$ERR"
+fi
